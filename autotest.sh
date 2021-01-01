1824f0d3c58b4f39d3ea1960153068cf757683eb
David P. Chassin <dchassin@slac.stanford.edu>
2021-01-01 07:49:51 -0800

    Update autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -x
+set -x 
 export OPENFIDO_INPUT
 export OPENFIDO_OUTPUT
 for OPENFIDO_INPUT in $(find $PWD/autotest -name 'input_*' -type d -print -prune); do
 	OPENFIDO_OUTPUT=$PWD/autotest/output_${OPENFIDO_INPUT##*_}
 	rm -rf $OPENFIDO_OUTPUT
 	mkdir $OPENFIDO_OUTPUT
-	$PWD/openfido.sh </dev/null 2>/dev/stdout 1>$OPENFIDO_OUTPUT/stdout | tee $OPENFIDO_OUTPUT/stderr
+	touch $OPENFIDO_OUTPUT/stderr
+	tail -f $OPENFIDO_OUTPUT/stderr &
+	sh openfido.sh </dev/null 1>$OPENFIDO_OUTPUT/stdout 2>$OPENFIDO_OUTPUT/stderr
+	kill %1
 done
